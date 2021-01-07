6a30e74852a78add5e1cb211b485a7c1e5d45495
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-07 05:47:13 -0300

    String part I

---
+++ JAVASCRIPT/w3scholl/string/scropt.js
@@ -0,0 +1,8 @@
+function mostrarTamanho(x) {
+    var txt = x.length;
+    document.getElementById("demo").innerHTML = txt
+}
+
+function mostrarAspas() {
+    document.getElementById("demo").innerHTML = " bom dia \" kkkk \" "
+}
