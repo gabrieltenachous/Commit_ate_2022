5a533324205c3b1caded2abd57401606679cece7
tenachous <gabriel_oliveira_f_sousa@outlook.com>
2021-01-07 08:42:50 -0300

    String part II

--- JAVASCRIPT/w3scholl/string/scropt.js
+++ JAVASCRIPT/w3scholl/string/scropt.js
@@ -6,3 +6,13 @@ function mostrarTamanho(x) {
 function mostrarAspas() {
     document.getElementById("demo").innerHTML = " bom dia \" kkkk \" "
 }
+
+function mostrarString() { 
+    console.log(
+        "Utilizando \\b: Gabriel \b Oliveira \n" +
+        "Utilizando \\f: Gabriel\fOliveira \n" +
+        "Utilizando \\n: Gabriel\nOliveira \n" +
+        "Utilizando \\r: Gabriel\rOliveira \n" +
+        "Utilizando \\t: Gabriel\tOliveira \n" +
+        "Utilizando \\v: Gabriel\vOliveira \n");
+}
