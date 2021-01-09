00283c5d187e1c7e1a0094d3ba50481686e0ed94
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-08 21:23:44 -0300

    Number part I

---
+++ JAVASCRIPT/w3scholl/number/script1.js
@@ -0,0 +1,23 @@
+/*
+1)Trabalhe com expoentes retornando 123000000 e 0.00123
+2)Deixe o resultado 0.2+0.1 100% preciso!
+3)Tente transformar o resultado em 30:
+var x = 10;
+var y = 20;
+var z = "The result is: " + x + y;
+4)Tente disparar 2 Not a number diferente
+5)Faça uma multiplicação em loop em que chegue no infinito
+6)Utilizo metodo numerico que torne da base 10 para base 2,8,16,32,10
+ */
+
+ function myFunction1(){
+     var a = 123e6;
+     var b = 12e-3;
+     document.getElementById("demo").innerHTML = a + " "+ b;
+ }
+
+ function myFunction2(){
+    var a = 123e6;
+    var b = 12e-3;
+    document.getElementById("demo").innerHTML = a + " "+ b;
+}
