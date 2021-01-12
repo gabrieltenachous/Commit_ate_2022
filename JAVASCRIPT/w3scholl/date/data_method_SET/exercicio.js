2bf7216befbabd573aac69bb7cb71b2ea06067dc
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-12 06:44:15 -0300

    data_method_set

---
+++ JAVASCRIPT/w3scholl/date/data_method_SET/exercicio.js
@@ -0,0 +1,15 @@
+/**setDate()	Set the day as a number (1-31)
+setFullYear()	Set the year (optionally month and day)
+setHours()	Set the hour (0-23)
+setMilliseconds()	Set the milliseconds (0-999)
+setMinutes()	Set the minutes (0-59)
+setMonth()	Set the month (0-11)
+setSeconds()	Set the seconds (0-59)
+setTime()	Set the time (milliseconds since January 1, 1970)
+ */
+
+//A unica diferença que agora voce insere os valores!!
+
+var a = new Date();
+a.setFullYear(2001);
+console.log(a)
