7705be1e85c7cb04c957fc095a2f721875521653
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-14 18:20:38 -0300

    Array e defined

--- PHP/udemy_php7.0/exemplo8_1.php
+++ PHP/udemy_php7.0/exemplo8_1.php
@@ -1,5 +1,19 @@
 <?php
 require "exemplo8.php";
 $resultado = somar(10,20);
-echo $resultado;
+echo $resultado; 
+$nomes = array();
+array_push($nomes,array(
+    'nome'=>'João',
+    'idade'=>20
+));
+array_push($nomes,array(
+    'nome'=>'Felipe',
+    'idade'=>32
+));
+echo'<br>';
+echo json_encode($nomes);
+$json = '[{"nome":"Jo\u00e3o","idade":20},{"nome":"Felipe","idade":32}]';
+$dado =  json_decode($json,true); 
+var_dump($dado);
 ?>
