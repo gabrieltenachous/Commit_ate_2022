0b8c548b747fda78b4a47dd65fdddc3c9ecd3c9b
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-14 10:45:45 -0300

    php Global

---
+++ PHP/udemy_php7.0/exemplo5.php
@@ -0,0 +1,15 @@
+<?php
+$nome = "Glaucio";
+function teste(){
+    global $nome;
+    echo $nome;
+
+}
+function teste2(){
+    global $nome;
+    echo $nome . "agora no teste 2";
+}
+
+teste();
+teste2();
+?>
