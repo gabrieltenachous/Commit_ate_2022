68ed407ccfb7e5ab440ed0de35b40c797d7591b2
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-02-05 08:16:19 -0300

    react

---
+++ React_native/NovoProjeto/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import X,{ Comp1, Comp2 } from './components/Multi'
+
+import Component from './components/Primeiro'
+export default () => (
+    <View>
+        <Comp1/>
+        <X></X>
+    </View>
+)
